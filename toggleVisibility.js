f67df656e9c8499ba83df55e57cac572618e6858
colonelgrek <60154949+colonelgrek@users.noreply.github.com>
2020-02-03 23:02:02 +0300

    дублирующийся файл

--- toggleVisibility.js
+++
@@ -1,9 +0,0 @@
-let button = document.getElementsByClassName("button-solution");
-let codeElement = document.querySelectorAll("code");
-for (let i = 0, len = codeElement.length; i < len; i++) {
-    button[i].addEventListener("click", function() {
-        if (codeElement[i].classList.contains("solution-hide")) {
-            codeElement[i].classList.remove("solution-hide");
-        } else {codeElement[i].classList.add("solution-hide");}
-    });
-}
