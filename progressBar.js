126021afcf70d1691527bbb43235143e6ebcf31e
colonelgrek <60154949+colonelgrek@users.noreply.github.com>
2020-02-03 23:01:45 +0300

    дублирующийся файл

--- progressBar.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    $(window).on("scroll resize", function() {
-        var o = $(window).scrollTop() / ($(document).height() - $(window).height());
-        $(".progress-bar").css({
-            "width": (100 * o | 0) + "%"
-        });
-        $('progress')[0].value = o;
-    })
-});
