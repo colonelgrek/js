3af453cb9970866b13ee29d8b72c96dfa4923ee9
colonelgrek <60154949+colonelgrek@users.noreply.github.com>
2020-02-03 23:01:10 +0300

    дублирующийся файл

--- playSolution.js
+++
@@ -1,6 +0,0 @@
-let buttonPlay = document.getElementsByClassName("button-play");
-for (let i = 0, len = buttonPlay.length; i < len; i++) {
-    buttonPlay[i].addEventListener("click", function() {
-        tasks[buttonPlay[i].dataset.name]();
-    }); 
-}
